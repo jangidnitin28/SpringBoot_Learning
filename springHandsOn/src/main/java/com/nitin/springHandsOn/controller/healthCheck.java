912321e995d7e5b239cf5bb9a0773409b6ff6a07
Nitin Jangid <jangidnitin282006@gmail.com>
2026-02-08 16:56:34 +0530

    Spring Boot API

---
+++ springHandsOn/src/main/java/com/nitin/springHandsOn/controller/healthCheck.java
@@ -0,0 +1,13 @@
+package com.nitin.springHandsOn.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class healthCheck {
+
+    @GetMapping("/health")
+    public String healthChecking() {
+        return "App is healthy";
+    }
+}
---
+++ springHandsOn/src/test/java/com/nitin/springHandsOn/SpringHandsOnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitin.springHandsOn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringHandsOnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
